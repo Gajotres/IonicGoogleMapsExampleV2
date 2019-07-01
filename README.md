c83579a6355f5724fc3af5068afa2e0a9b0ee31a
Dragan Gaic <dragan.gaic@allianz.at>
2019-07-01 21:09:54 +0530

    DG: The first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# IonicGoogleMapsExampleV2
+### This is a Ionic 4 tutorial example made for https://www.gajotres.net/ionic-2-having-fun-with-cordova-geolocation-plugin/
+
+Follow these instruction to deploy this example:
+
+1. git clone https://github.com/Gajotres/IonicGoogleMapsExampleV2.git
+2. cd IonicGoogleMapsExampleV2
+3. ionic cordova platform add browser
+4. npm install
+5. ionic cordova run browser
+
+An old version (Ionic 2) of this tutorial can be found here: https://github.com/Gajotres/Ionic2RESTHttpExample
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,65 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps/ngx';
+import { Platform } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  map: GoogleMap;
+
+  constructor(private platform: Platform) {}
+
+  async ngOnInit() {
+  	await this.platform.ready();
+    await this.loadMap();
+  } 
+
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': '<ADD YOUR API KEY>',
+      'API_KEY_FOR_BROWSER_DEBUG': '<ADD YOUR API KEY>'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }  
+
+}
